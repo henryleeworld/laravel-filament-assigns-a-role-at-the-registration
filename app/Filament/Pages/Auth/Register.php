2f8ff9fe1937df3a11e1a35ce607690352041e46
Henry Lee <henryleeworld@gmail.com>
2023-09-15 00:08:11 +0800

    feat:初始化

---
+++ app/Filament/Pages/Auth/Register.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Filament\Pages\Auth;
+
+use DanHarrin\LivewireRateLimiting\Exceptions\TooManyRequestsException;
+use Filament\Facades\Filament;
+use Filament\Forms\Components\Component;
+use Filament\Forms\Components\Select;
+use Filament\Http\Responses\Auth\Contracts\RegistrationResponse;
+use Filament\Notifications\Notification;
+use Filament\Pages\Auth\Register as BaseRegister;
+use Illuminate\Auth\Events\Registered;
+
+class Register extends BaseRegister
+{
+    public function register(): ?RegistrationResponse
+    {
+        try {
+            $this->rateLimit(2);
+        } catch (TooManyRequestsException $exception) {
+            Notification::make()
+                ->title(__('filament-panels::pages/auth/register.notifications.throttled.title', [
+                    'seconds' => $exception->secondsUntilAvailable,
+                    'minutes' => ceil($exception->secondsUntilAvailable / 60),
+                ]))
+                ->body(array_key_exists('body', __('filament-panels::pages/auth/register.notifications.throttled') ?: []) ? __('filament-panels::pages/auth/register.notifications.throttled.body', [
+                    'seconds' => $exception->secondsUntilAvailable,
+                    'minutes' => ceil($exception->secondsUntilAvailable / 60),
+                ]) : null)
+                ->danger()
+                ->send();
+
+            return null;
+        }
+
+        $data = $this->form->getState();
+        // $data['role'] = 'user';
+
+        $user = $this->getUserModel()::create($data);
+
+        app()->bind(
+            \Illuminate\Auth\Listeners\SendEmailVerificationNotification::class,
+            \Filament\Listeners\Auth\SendEmailVerificationNotification::class,
+        );
+        event(new Registered($user));
+
+        Filament::auth()->login($user);
+
+        session()->regenerate();
+
+        return app(RegistrationResponse::class);
+    }
+
+    protected function getForms(): array
+    {
+        return [
+            'form' => $this->form(
+                $this->makeForm()
+                    ->schema([
+                        $this->getNameFormComponent(),
+                        $this->getEmailFormComponent(),
+                        $this->getPasswordFormComponent(),
+                        $this->getPasswordConfirmationFormComponent(),
+                        $this->getRoleFormComponent(),
+                    ])
+                    ->statePath('data'),
+            ),
+        ];
+    }
+
+    protected function getRoleFormComponent(): Component
+    {
+        return Select::make('role')
+            ->label(__('Role'))
+            ->options([
+                'buyer' => __('Buyer'),
+                'seller' => __('Seller'),
+            ])
+            ->default('buyer')
+            ->required();
+    }
+}
